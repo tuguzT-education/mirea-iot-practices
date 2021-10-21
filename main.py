b6dee29ccf6b4cd5b17e24117a1619221fa32c08
tuguzT <timurka.tugushev@gmail.com>
2021-10-21 12:00:39 +0300

    Added code for topic subscription and publishing

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import mqtt
 
 def main():
     client = mqtt.connect()
-    client.loop_forever()
+    mqtt.subscribe(client, device='wb-msw-v3_21', control='Temperature')
+    client.loop(timeout=2)
+    mqtt.unsubscribe(client, device='wb-msw-v3_21', control='Temperature')
 
 
 if __name__ == '__main__':
--- mqtt.py
+++ mqtt.py
@@ -12,19 +12,58 @@ password: Final[str] = 'wirenboard'
 def connect() -> mqtt_client.Client:
     # noinspection PyUnusedLocal
     def on_connect(client: mqtt_client.Client, userdata: Any, flags: Any, rc: int):
-        if rc == 0:
-            print("Connected to the MQTT Broker!")
-        else:
-            print(f"Failed to connect, return code {rc}")
+        result = mqtt_client.connack_string(rc)
+        print(f'Connected with result "{result}"')
+
+    # noinspection PyUnusedLocal
+    def on_disconnect(client: mqtt_client.Client, userdata: Any, rc: int):
+        print('Disconnected')
 
     # noinspection PyUnusedLocal
     def on_message(client: mqtt_client.Client, userdata: Any, message: mqtt_client.MQTTMessage):
-        print(f"{message.topic} {str(message.payload)}")
+        print(f'The message received: topic is "{message.topic}", payload is "{str(message.payload)}"')
 
     new_client = mqtt_client.Client(client_id)
     new_client.username_pw_set(username, password)
     new_client.on_connect = on_connect
+    new_client.on_disconnect = on_disconnect
     new_client.on_message = on_message
 
     new_client.connect(host, port)
     return new_client
+
+
+def raise_error(error_code: int):
+    if error_code != mqtt_client.MQTT_ERR_SUCCESS:
+        error = mqtt_client.error_string(error_code)
+        if error != "Unknown error.":
+            raise Exception(error)
+
+
+def generate_topic(device: str, control: str) -> str:
+    return f'/devices/{device}/controls/{control}'
+
+
+def publish(self: mqtt_client.Client, device: str, control: str, payload: str):
+    topic = generate_topic(device, control)
+
+    result: mqtt_client.MQTTMessageInfo = self.publish(topic, payload)
+    raise_error(result.rc)
+
+
+def subscribe(self: mqtt_client.Client, device: str, control: str):
+    topic = generate_topic(device, control)
+
+    rc: int
+    message_id: int | None
+    rc, message_id = self.subscribe(topic)
+    raise_error(rc)
+
+
+def unsubscribe(self: mqtt_client.Client, device: str, control: str):
+    topic = generate_topic(device, control)
+
+    rc: int
+    message_id: int | None
+    rc, message_id = self.unsubscribe(topic)
+    raise_error(rc)
