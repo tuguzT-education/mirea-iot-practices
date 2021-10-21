ae18a4d1cc6db9d07ea60c08f01da1fc90db205a
tuguzT <timurka.tugushev@gmail.com>
2021-10-21 12:29:31 +0300

    Move logic into class

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import mqtt
 
 
 def main():
-    client = mqtt.connect()
-    mqtt.subscribe(client, device='wb-msw-v3_21', control='Temperature')
+    client = mqtt.Client()
+    client.subscribe(device='wb-msw-v3_21', control='Temperature')
     client.loop(timeout=2)
-    mqtt.unsubscribe(client, device='wb-msw-v3_21', control='Temperature')
+    client.unsubscribe(device='wb-msw-v3_21', control='Temperature')
 
 
 if __name__ == '__main__':
--- mqtt.py
+++ mqtt.py
@@ -2,68 +2,83 @@ from typing import Final, Any
 
 from paho.mqtt import client as mqtt_client
 
-client_id: Final[str] = 'iot_practice_7'
-host: Final[str] = '192.168.1.15'
-port: Final[int] = 22
-username: Final[str] = 'root'
-password: Final[str] = 'wirenboard'
 
+class Client(object):
+    def __init__(self):
+        self.__client_id: Final[str] = 'iot_practice_7'
+        self.__host: Final[str] = '192.168.1.15'
+        self.__port: Final[int] = 22
+        self.__username: Final[str] = 'root'
+        self.__password: Final[str] = 'wirenboard'
 
-def connect() -> mqtt_client.Client:
+        self.__client = mqtt_client.Client(self.__client_id)
+        self.__client.username_pw_set(self.__username, self.__password)
+        self.__client.on_connect = self.__on_connect
+        self.__client.on_disconnect = self.__on_disconnect
+        self.__client.on_message = self.__on_message
+        self.__client.connect(self.__host, self.__port)
+
+    def publish(self, device: str, control: str, payload: str):
+        topic = generate_topic(device, control)
+
+        result: mqtt_client.MQTTMessageInfo = self.__client.publish(topic, payload)
+        self.__raise_error_if_any(result.rc)
+
+    def subscribe(self, device: str, control: str):
+        topic = generate_topic(device, control)
+
+        result_code: int
+        message_id: int | None
+        result_code, message_id = self.__client.subscribe(topic)
+        self.__raise_error_if_any(result_code)
+
+    def unsubscribe(self, device: str, control: str):
+        topic = generate_topic(device, control)
+
+        result_code: int
+        message_id: int | None
+        result_code, message_id = self.__client.unsubscribe(topic)
+        self.__raise_error_if_any(result_code)
+
+    def loop(self, timeout: int):
+        result_code = self.__client.loop(timeout)
+        self.__raise_error_if_any(result_code)
+
     # noinspection PyUnusedLocal
-    def on_connect(client: mqtt_client.Client, userdata: Any, flags: Any, rc: int):
+    @staticmethod
+    def __on_connect(client: mqtt_client.Client, userdata: Any, flags: Any, rc: int):
         result = mqtt_client.connack_string(rc)
         print(f'Connected with result "{result}"')
 
     # noinspection PyUnusedLocal
-    def on_disconnect(client: mqtt_client.Client, userdata: Any, rc: int):
+    @staticmethod
+    def __on_disconnect(client: mqtt_client.Client, userdata: Any, rc: int):
         print('Disconnected')
 
     # noinspection PyUnusedLocal
-    def on_message(client: mqtt_client.Client, userdata: Any, message: mqtt_client.MQTTMessage):
+    @staticmethod
+    def __on_message(client: mqtt_client.Client, userdata: Any, message: mqtt_client.MQTTMessage):
         print(f'The message received: topic is "{message.topic}", payload is "{str(message.payload)}"')
 
-    new_client = mqtt_client.Client(client_id)
-    new_client.username_pw_set(username, password)
-    new_client.on_connect = on_connect
-    new_client.on_disconnect = on_disconnect
-    new_client.on_message = on_message
-
-    new_client.connect(host, port)
-    return new_client
+    @staticmethod
+    def __raise_error_if_any(error_code: int):
+        if error_code != mqtt_client.MQTT_ERR_SUCCESS:
+            error = mqtt_client.error_string(error_code)
+            if error != "Unknown error.":
+                raise MQTTException(error)
 
 
-def raise_error(error_code: int):
-    if error_code != mqtt_client.MQTT_ERR_SUCCESS:
-        error = mqtt_client.error_string(error_code)
-        if error != "Unknown error.":
-            raise Exception(error)
+class MQTTException(Exception):
+    def __init__(self, message: str):
+        self.__message = message
+        super().__init__(self.__message)
+
+    def __str__(self) -> str:
+        return f'MQTT exception: "{self.__message}"'
+
+    def message(self) -> str:
+        return self.__message
 
 
 def generate_topic(device: str, control: str) -> str:
     return f'/devices/{device}/controls/{control}'
-
-
-def publish(self: mqtt_client.Client, device: str, control: str, payload: str):
-    topic = generate_topic(device, control)
-
-    result: mqtt_client.MQTTMessageInfo = self.publish(topic, payload)
-    raise_error(result.rc)
-
-
-def subscribe(self: mqtt_client.Client, device: str, control: str):
-    topic = generate_topic(device, control)
-
-    rc: int
-    message_id: int | None
-    rc, message_id = self.subscribe(topic)
-    raise_error(rc)
-
-
-def unsubscribe(self: mqtt_client.Client, device: str, control: str):
-    topic = generate_topic(device, control)
-
-    rc: int
-    message_id: int | None
-    rc, message_id = self.unsubscribe(topic)
-    raise_error(rc)
